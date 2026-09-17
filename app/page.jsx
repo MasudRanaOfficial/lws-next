3d93cc3096dce0f9ecf7e614adaee4bb2de8aa10
Masud Rana <masudrana.arh@gmail.com>
2026-09-17 10:54:01 +0600

    Add layout and home page components with initial styling

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="mt-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+        doloribus ratione accusantium dignissimos eos ipsa esse nostrum?
+        Delectus dolorem quidem maiores assumenda fugiat eaque veritatis dicta.
+        Exercitationem ipsum facere est!
+      </h2>
+    </div>
+  );
+}
